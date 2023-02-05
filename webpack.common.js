7249dfee736173d5a6702702cc45f80e5d4e61d6
Paras Santra <phantom@parass-air.home>
2023-02-05 13:03:52 -0500

    add html-loader assests module and clean webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,14 @@ module.exports={
             {
                 test: /\.scss$/,
                 use: ['style-loader','css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
             }
         ]
     }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,14 @@
 const path = require('path')
 const common = require("./webpack.common");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
     mode: 'production',
     output: {
         filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[name].[hash][ext][query]'
+    },
+    plugins: [new CleanWebpackPlugin()],
 })
