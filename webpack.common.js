3e67458e0073e984a8eb81f9f91890168588c726
Paras Santra <phantom@parass-air.home>
2023-02-05 02:38:11 -0500

    add webpack comon dev and prd config

---
+++ webpack.common.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports={
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+    ],
+    module:{
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader','css-loader', 'sass-loader']
+            }
+        ]
+    }
+}
