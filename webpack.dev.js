2f50f4fba1cc9a893fba466cc106a28528909d4c
Paras Santra <phantom@parass-air.home>
2023-02-05 13:30:46 -0500

    add vendor js entry point and bootstrap javascript

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
         static: './dist',
     },
     output: {
-        filename: "main.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     }
 })
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const { merge } = require('webpack-merge');
 module.exports = merge(common, {
     mode: 'production',
     output: {
-        filename: "main.[contenthash].js",
+        filename: "[name].[contenthash].bundle.js",
         path: path.resolve(__dirname, 'dist'),
         assetModuleFilename: 'images/[name].[hash][ext][query]'
     },
