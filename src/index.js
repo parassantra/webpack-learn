1682309aa918b150f61783ce7115aa0246db0b5c
Paras Santra <phantom@parass-air.home>
2023-02-05 01:54:05 -0500

    add sass-loader and override bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import AlertService from "./app/alert.service";
 import ComponentService from "./app/component.service";
-import './main.css'
+import './main.scss'
 import {run} from './app/app'
 const alertService = new AlertService()
 const componentService = new ComponentService()
